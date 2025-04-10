e515665a74f80e71f31f0a6240377202a4e16719
ikermendezz <iker.mendez@estudiantat.upc.edu>
2025-04-10 17:06:30 +0200

    Add ./ file

--- tst.py
+++ tst.py
@@ -1,2 +1,12 @@
 print('Hola')
 print('Adios')
+latitude=23
+name='juancarlos'
+class Point:
+    def __int__(self,name,age):
+        self.latitude=latitude
+        self.name=name
+        self.age(23)
+class Camino:
+    def __int__(self,Point,vector):
+        self.vector=Point1-Point2
